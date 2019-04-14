0ec063633f808c5f4143cf5a9fa23fdbfa94d78a
User <user@rba-hackerrank.com>
2019-04-14 13:17:12 +0000

    [HackerRank] Auto commit to save code

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { userActions } from '../actions'
 
-import { userActions } from '../actions';
-
-export class LoginPage extends Component {
+class LoginPage extends Component {
     constructor(props) {
         super(props);
 
@@ -22,10 +22,34 @@ export class LoginPage extends Component {
     }
 
     handleChange(e) {
+        const { name, value } = e.target;
+        if(name){
+            if(name === 'username'){
+               this.setState({
+                   username: value
+               }) 
+            }
+            if(name === 'password'){
+               this.setState({
+                   password: value
+               }) 
+            }
+        }
     }
 
     handleSubmit(e) {
-        
+        e.preventDefault();
+        const { username, password } = this.state;
+        if(username && password){
+            console.log(this.props)
+            this.props.loginUser();
+        }else {
+            
+        }
+        this.setState({
+            submitted: true
+        })
+      
     }
 
     render() {
@@ -33,17 +57,17 @@ export class LoginPage extends Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
-                <form name="form">
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input onChange={this.handleChange} type="text" className="form-control username" name="username" />
                         {submitted && !username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input onChange={this.handleChange} type="password" className="form-control" name="password"/>
                         {submitted && !password &&
                             <div className="help-block">Password is required</div>
                         }
@@ -61,4 +85,13 @@ function mapStateToProps(state) {
 
 }
 
-export { LoginPage as TestLoginPage };
+function mapDispatchToProps(dispatch) {
+    console.log(121, 'dispatch called')
+    return {
+        loginUser: bindActionCreators(userActions.login, dispatch)
+    }
+
+}
+
+export  { connect(mapStateToProps, mapDispatchToProps)(LoginPage) };
+
